d20ad33bac446ba800ac9ef25fabb1113300ab09
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2014-10-28 21:24:54 -0500

    Revert "Spaces, not tabs"
    This reverts commit 9e1496b6bb8476bda3825854b5a4f4f8c0d26da3.

--- Log.cpp
+++ Log.cpp
@@ -24,166 +24,166 @@ using namespace std;
 
 namespace neosmart
 {
-    __thread int IndentLevel = -1;
+	__thread int IndentLevel = -1;
 
-    Logger logger = Logger();
+	Logger logger = Logger();
 
-    LPCTSTR logLevelNames[] = {_T("DEBG"), _T("INFO"), _T("WARN"), _T("ERRR")};
+	LPCTSTR logLevelNames[] = {_T("DEBG"), _T("INFO"), _T("WARN"), _T("ERRR")};
 
-    Logger::Logger(LogLevel logLevel)
-    {
-        _logLevel = logLevel;
+	Logger::Logger(LogLevel logLevel)
+	{
+		_logLevel = logLevel;
 #ifdef WIN32
-        AddLogDestination(std::wcout);
+		AddLogDestination(std::wcout);
 #else
-        AddLogDestination(std::cout);
+		AddLogDestination(std::cout);
 #endif
-        _consoleOnly = true;
-    }
+		_consoleOnly = true;
+	}
 
-    void Logger::InnerLog(LogLevel level, LPCTSTR message, va_list params)
-    {
-        if(level < _logLevel)
-            return;
+	void Logger::InnerLog(LogLevel level, LPCTSTR message, va_list params)
+	{
+		if(level < _logLevel)
+			return;
 
-        TCHAR *mask;
+		TCHAR *mask;
 
-        //Indentation only works if ScopeLog is printing 
-        size_t size = 4 + 2 + _tcsclen(message) + 2 + 1;
-        if(IndentLevel >= 0 && _logLevel <= neosmart::Debug)
-        {
-            size += (size_t) IndentLevel;
-            mask = new TCHAR[size];
-            _stprintf_s(mask, size, _T("%*s: %s\r\n"), IndentLevel + 4, logLevelNames[level], message);
-        }
-        else
-        {
-            mask = new TCHAR[size];
-            _stprintf_s(mask, size, _T("%s: %s\r\n"), logLevelNames[level], message);
-        }
+		//Indentation only works if ScopeLog is printing 
+		size_t size = 4 + 2 + _tcsclen(message) + 2 + 1;
+		if(IndentLevel >= 0 && _logLevel <= neosmart::Debug)
+		{
+			size += (size_t) IndentLevel;
+			mask = new TCHAR[size];
+			_stprintf_s(mask, size, _T("%*s: %s\r\n"), IndentLevel + 4, logLevelNames[level], message);
+		}
+		else
+		{
+			mask = new TCHAR[size];
+			_stprintf_s(mask, size, _T("%s: %s\r\n"), logLevelNames[level], message);
+		}
 
-        if(_consoleOnly)
-        {
-            vwprintf_s(mask, params);
-        }
-        else
-        {
+		if(_consoleOnly)
+		{
+			vwprintf_s(mask, params);
+		}
+		else
+		{
 #ifdef WIN32
-            size_t length = _vsntprintf(NULL, 0, mask, params);
+			size_t length = _vsntprintf(NULL, 0, mask, params);
 #else
-            //See http://stackoverflow.com/questions/8047362/is-gcc-mishandling-a-pointer-to-a-va-list-passed-to-a-function
-            va_list args_copy;
-            va_copy(args_copy, params);
-            size_t length = _vsntprintf(NULL, 0, mask, args_copy);
-            va_end(args_copy);
+			//See http://stackoverflow.com/questions/8047362/is-gcc-mishandling-a-pointer-to-a-va-list-passed-to-a-function
+			va_list args_copy;
+			va_copy(args_copy, params);
+			size_t length = _vsntprintf(NULL, 0, mask, args_copy);
+			va_end(args_copy);
 #endif
-            TCHAR *final = new TCHAR [length + 1];
-            _vsntprintf(final, length + 1, mask, params);
+			TCHAR *final = new TCHAR [length + 1];
+			_vsntprintf(final, length + 1, mask, params);
 
-            for(vector<ostream*>::iterator i = _outputs.begin(); i != _outputs.end(); ++i)
-            {
-                (**i) << final;
-            }
+			for(vector<ostream*>::iterator i = _outputs.begin(); i != _outputs.end(); ++i)
+			{
+				(**i) << final;
+			}
 
-            delete [] final;
-        }
+			delete [] final;
+		}
 
-        delete [] mask;
-    }
+		delete [] mask;
+	}
 
-    void Logger::Log(LogLevel level, LPCTSTR message, ...)
-    {
-        va_list va_args;
-        va_start(va_args, message);
-        InnerLog(level, message, va_args);
-        va_end(va_args);
-    }
+	void Logger::Log(LogLevel level, LPCTSTR message, ...)
+	{
+		va_list va_args;
+		va_start(va_args, message);
+		InnerLog(level, message, va_args);
+		va_end(va_args);
+	}
 
-    void Logger::Log(LPCTSTR message, ...)
-    {
-        va_list va_args;
-        va_start(va_args, message);
-        InnerLog(neosmart::Info, message, va_args);
-        va_end(va_args);
-    }
+	void Logger::Log(LPCTSTR message, ...)
+	{
+		va_list va_args;
+		va_start(va_args, message);
+		InnerLog(neosmart::Info, message, va_args);
+		va_end(va_args);
+	}
 
-    void Logger::Debug(LPCTSTR message, ...)
-    {
-        va_list va_args;
-        va_start(va_args, message);
-        InnerLog(neosmart::Debug, message, va_args);
-        va_end(va_args);
-    }
+	void Logger::Debug(LPCTSTR message, ...)
+	{
+		va_list va_args;
+		va_start(va_args, message);
+		InnerLog(neosmart::Debug, message, va_args);
+		va_end(va_args);
+	}
 
-    void Logger::Info(LPCTSTR message, ...)
-    {
-        va_list va_args;
-        va_start(va_args, message);
-        InnerLog(neosmart::Info, message, va_args);
-        va_end(va_args);
-    }
+	void Logger::Info(LPCTSTR message, ...)
+	{
+		va_list va_args;
+		va_start(va_args, message);
+		InnerLog(neosmart::Info, message, va_args);
+		va_end(va_args);
+	}
 
-    void Logger::Warn(LPCTSTR message, ...)
-    {
-        va_list va_args;
-        va_start(va_args, message);
-        InnerLog(neosmart::Warn, message, va_args);
-        va_end(va_args);
-    }
+	void Logger::Warn(LPCTSTR message, ...)
+	{
+		va_list va_args;
+		va_start(va_args, message);
+		InnerLog(neosmart::Warn, message, va_args);
+		va_end(va_args);
+	}
 
-    void Logger::Error(LPCTSTR message, ...)
-    {
-        va_list va_args;
-        va_start(va_args, message);
-        InnerLog(neosmart::Error, message, va_args);
-        va_end(va_args);
-    }
+	void Logger::Error(LPCTSTR message, ...)
+	{
+		va_list va_args;
+		va_start(va_args, message);
+		InnerLog(neosmart::Error, message, va_args);
+		va_end(va_args);
+	}
 
-    void Logger::SetLogLevel(LogLevel logLevel)
-    {
-        _logLevel = logLevel;
-    }
+	void Logger::SetLogLevel(LogLevel logLevel)
+	{
+		_logLevel = logLevel;
+	}
 
-    void Logger::AddLogDestination(neosmart::ostream &destination)
-    {
-        _consoleOnly = false;
-        _outputs.push_back(&destination);
-    }
+	void Logger::AddLogDestination(neosmart::ostream &destination)
+	{
+		_consoleOnly = false;
+		_outputs.push_back(&destination);
+	}
 
-    void Logger::ClearLogDestinations()
-    {
-        _consoleOnly = false;
-        _outputs.clear();
-    }
+	void Logger::ClearLogDestinations()
+	{
+		_consoleOnly = false;
+		_outputs.clear();
+	}
 
-    void ScopeLog::Initialize(LPCTSTR name)
-    {
-        _name = name;
-        ++IndentLevel;
-        logger.Log(Debug, _T("Entering %s"), _name);
-    }
+	void ScopeLog::Initialize(LPCTSTR name)
+	{
+		_name = name;
+		++IndentLevel;
+		logger.Log(Debug, _T("Entering %s"), _name);
+	}
 
-    ScopeLog::ScopeLog(LPCTSTR name)
-    {
-        _allocated = false;
-        Initialize(name);
-    }
+	ScopeLog::ScopeLog(LPCTSTR name)
+	{
+		_allocated = false;
+		Initialize(name);
+	}
 
 #ifdef _WIN32
-    ScopeLog::ScopeLog(LPCSTR name)
-    {
-        _allocated = true;
-        LPCTSTR newName = _tcsdup(CString(name));
-        Initialize(newName);
-    }
+	ScopeLog::ScopeLog(LPCSTR name)
+	{
+		_allocated = true;
+		LPCTSTR newName = _tcsdup(CString(name));
+		Initialize(newName);
+	}
 #endif
 
-    ScopeLog::~ScopeLog()
-    {
-        logger.Log(Debug, _T("Leaving %s"), _name);
-        --IndentLevel;
+	ScopeLog::~ScopeLog()
+	{
+		logger.Log(Debug, _T("Leaving %s"), _name);
+		--IndentLevel;
 
-        if(_allocated)
-            free((void*) _name);
-    }
+		if(_allocated)
+			free((void*) _name);
+	}
 }
